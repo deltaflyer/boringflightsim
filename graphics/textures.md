53545d49e933b30514b018bd66603542fbb149ca
Oliver Wannenwetsch <github@wannenwets.ch>
2016-02-28 20:53:13 +0100

    background pictures added

--- graphics/textures.md
+++ graphics/textures.md
@@ -7,3 +7,5 @@ cloud2.png - http://opengameart.org/content/2d-clouds
 cloud3.png - http://opengameart.org/content/2d-clouds
 icon.png - http://icon-icons.com/de/symbol/Flug-Flugzeug-Flugzeug/30822
 gras.png - http://opengameart.org/content/seamless-grass-textures-20-pack
+buildings-layer.ong - http://opengameart.org/content/urban-landscape
+sky-layer.png - http://opengameart.org/content/urban-landscape
--- scenery.py
+++ scenery.py
@@ -13,6 +13,7 @@ class Scenery(pygame.sprite.Sprite):
 		self.plane = plane
 
 	def update(self,):
+		self.__print_background()
 		self.__print_sky()
 		self.__print_grass()
 
@@ -23,7 +24,6 @@ class Scenery(pygame.sprite.Sprite):
 		# if the plane is too high, then print no gras
 		if self.plane.get_y_coords() < 400:
 			return
-
 		x = 0
 		correction_y = 0
 		gras = self.__get_gras()
@@ -45,4 +45,29 @@ class Scenery(pygame.sprite.Sprite):
 		return (gras_img, gras_rect)
 
 	def __print_sky(self):
+		# if the plane is too high, then print no gras
+		if self.plane.get_y_coords() < 350:
+			return
+
+		x = 0
+		correction_y = 0
+		sky = self.__get_sky()
+
+		# Calculate the correction factor for y based on plane height
+		if (self.plane.get_y_coords()) < 450:
+			correction_y = self.plane.get_y_coords() - 450
+
+		# Print gras
+		sky[1].centery = 650 - correction_y
+		for i in range(1, 16):
+			sky[1].centerx = x
+			self.screen.blit(sky[0], sky[1])
+			x = x + 112
+
+	def __get_sky(self):
+		sky_img = pygame.image.load(os.path.join('graphics', 'sky-layer.png')).convert()
+		sky_rect = sky_img.get_rect()
+		return (sky_img, sky_rect)
+
+	def __print_background(self):
 		self.screen.fill((182, 211, 225))
